53617d8cdf6a25c3951f7bb2b4e3a3f1b0f1f232
djra26111990 <djra26111990@gmail.com>
2020-10-29 18:00:42 -0500

    Changes on controllers adding try/catch and typing functions with Promise<Response>

--- src/controllers/index.controller.ts
+++ src/controllers/index.controller.ts
@@ -13,55 +13,76 @@ export const getUsers = async (req: Request, res: Response): Promise<Response> =
         return res.status(200).json(response.rows);
     } catch (e) {
         console.log(e);
-        return res.status(500).json('Internal Server error');
+        return res.status(500).json('Internal Server Error');
     }
 };
 
 //Se obtiene un único usuario por su Id
 export const getUserById = async (req: Request, res: Response): Promise<Response> => {
-    const id = parseInt(req.params.id);
-    const response: QueryResult = await pool.query('SELECT * FROM users WHERE id = $1', [id]);
-    return res.json(response.rows);
+    try {
+        const id = parseInt(req.params.id);
+        const response: QueryResult = await pool.query('SELECT * FROM users WHERE id = $1', [id]);
+        return res.status(200).json(response.rows);
+    } catch(e) {
+        console.log(e);
+        return res.status(500).json('Internal Server Error');
+    }
+    
 };
 
 //Permite la creación de un usuario
-export const createUser = async (req: Request, res: Response) => {
-    const { username, password, name, email } = req.body;
+export const createUser = async (req: Request, res: Response): Promise<Response> => {
+    try {
+        const { username, password, name, email } = req.body;
     const passwordHashed = await bcryptjs.hash(password, roundSalt);
-    const response = await pool.query('INSERT INTO users (username, password, name, email) VALUES ($1, $2, $3, $4)', [username, passwordHashed, name, email]);
-    res.json({
+    await pool.query('INSERT INTO users (username, password, name, email) VALUES ($1, $2, $3, $4)', [username, passwordHashed, name, email]);
+    return res.status(200).json({
         message: 'User Added successfully',
         body: {
             user: { username, passwordHashed, name, email }
         }
-    })
+    });
+    } catch(e) {
+        console.log(e);
+        return res.status(500).json('Internal Server Error');
+    }
 };
 
 //Permite la actualización de datos de un usuario
-export const updateUser = async (req: Request, res: Response) => {
-    const id = parseInt(req.params.id);
-    const { username, password, name, email } = req.body;
-    const passwordHashed = await bcryptjs.hash(password, roundSalt);
-    const response = await pool.query('UPDATE users SET username = $1, password = $2, name = $3, email = $4 WHERE id = $5', [
-        username, 
-        passwordHashed, 
-        name, 
-        email,
-        id
-    ]);
-    res.json({
-        message: 'User updated succesfully',
-        body: {
-            user: { username, passwordHashed, name, email }
-        }
-    });
+export const updateUser = async (req: Request, res: Response): Promise<Response> => {
+    try {
+        const id = parseInt(req.params.id);
+        const { username, password, name, email } = req.body;
+        const passwordHashed = await bcryptjs.hash(password, roundSalt);
+        await pool.query('UPDATE users SET username = $1, password = $2, name = $3, email = $4 WHERE id = $5', [
+            username, 
+            passwordHashed, 
+            name, 
+            email,
+            id
+        ]);
+        return res.status(200).json({
+            message: 'User updated succesfully',
+            body: {
+                user: { username, passwordHashed, name, email }
+            }
+        });
+    } catch(e) {
+        console.log(e);
+        return res.status(500).json('Internal Error Server');
+    }
 };
 
 //Elimina un usuario por su Id
-export const deleteUser = async (req: Request, res: Response) => {
-    const id = parseInt(req.params.id);
-    await pool.query('DELETE FROM users where id = $1', [
-        id
-    ]);
-    res.json(`User ${id} deleted Successfully`);
+export const deleteUser = async (req: Request, res: Response): Promise<Response> => {
+    try {
+        const id = parseInt(req.params.id);
+        await pool.query('DELETE FROM users where id = $1', [
+            id
+        ]);
+        return res.json(`User ${id} deleted Successfully`);
+    } catch(e) {
+        console.log(e);
+        return res.status(500).json('Internal Error Server');
+    }
 };
